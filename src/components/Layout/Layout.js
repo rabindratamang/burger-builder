7e69c553398558cb7e8e1e4966cafe38a4a0965a
Rabindra Tamang <rabindratamang147@gmail.com>
2020-06-05 12:59:41 +0545

    Add SideDrawer component and add responsive css

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = props => {
   return (
     <Fragment>
+      <SideDrawer />
       <Toolbar />
       <div>toolbar, sidebar, backdrop</div>
       <main className={classes.content}>{props.children}</main>
