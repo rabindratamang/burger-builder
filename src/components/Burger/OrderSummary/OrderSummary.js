78661dc5993305fa542455e804c4bcaf448f2db8
Rabindra Tamang <rabindratamang147@gmail.com>
2020-06-05 14:39:47 +0545

    Minor refactor on modal component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,33 +1,35 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
+class OrderSummary extends Component {
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+      return (
+        <li key={igKey}>
+          <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
+          {this.props.ingredients[igKey]}
+        </li>
+      );
+    });
+
     return (
-      <li key={igKey}>
-        <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
-        {props.ingredients[igKey]}
-      </li>
+      <Fragment>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total price: $ {this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to checkout?</p>
+        <Button clicked={this.props.purchaseCanceled} btnType="Danger">
+          CANCEL
+        </Button>
+        <Button clicked={this.props.purchaseContinued} btnType="Success">
+          CONTINUE
+        </Button>
+      </Fragment>
     );
-  });
+  }
+}
 
-  return (
-    <Fragment>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total price: $ {props.price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to checkout?</p>
-      <Button clicked={props.purchaseCanceled} btnType="Danger">
-        CANCEL
-      </Button>
-      <Button clicked={props.purchaseContinued} btnType="Success">
-        CONTINUE
-      </Button>
-    </Fragment>
-  );
-};
-
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,33 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import classes from './Modal.css';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = props => (
-  <Fragment>
-    <Backdrop show={props.showModal} clicked={props.modalClosed} />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.showModal ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.showModal ? '1' : '0',
-      }}
-    >
-      {props.showModal ? props.children : null}
-    </div>
-  </Fragment>
-);
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
 
-export default modal;
+  render() {
+    return (
+      <Fragment>
+        <Backdrop
+          show={this.props.showModal}
+          clicked={this.props.modalClosed}
+        />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.showModal
+              ? 'translateY(0)'
+              : 'translateY(-100vh)',
+            opacity: this.props.showModal ? '1' : '0',
+          }}
+        >
+          {this.props.showModal ? this.props.children : null}
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default Modal;
