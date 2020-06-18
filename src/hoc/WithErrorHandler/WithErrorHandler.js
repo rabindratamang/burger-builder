810aecd317254989dd34a8e2d8d604b9891d9808
Rabindra Tamang <rabindratamang147@gmail.com>
2020-06-18 12:00:39 +0545

    Add global error handler

---
+++ src/hoc/WithErrorHandler/WithErrorHandler.js
@@ -0,0 +1,40 @@
+import React, { Fragment, Component } from 'react';
+import Modal from '../../components/UI/Modal/Modal';
+
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends Component {
+    state = {
+      error: null,
+    };
+
+    componentDidMount() {
+      axios.interceptors.request.use(req => {
+        console.log(req);
+        this.setState({ error: null });
+        return req;
+      });
+      axios.interceptors.response.use(
+        res => res,
+        error => {
+          this.setState({ error: error });
+        }
+      );
+    }
+
+    render() {
+      return (
+        <Fragment>
+          <Modal
+            showModal={this.state.error}
+            modalClosed={() => this.setState({ error: null })}
+          >
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <WrappedComponent {...this.props} />
+        </Fragment>
+      );
+    }
+  };
+};
+
+export default withErrorHandler;
